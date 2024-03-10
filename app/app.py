82ab38527672be72784cd0b61af6688d7c167637
sentenzo <sentenzo@users.noreply.github.com>
2024-03-10 09:47:10 +0300

    project structure

--- app/app.py
+++ app/app.py
@@ -1,13 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 
 app = FastAPI()
 
 
 @app.get("/", response_class=FileResponse)
 async def page0() -> FileResponse:
-    return FileResponse(path="app/html/page_0.html", status_code=200)
+    return FileResponse(path="app/static/html/page_0.html", status_code=200)
 
 
 @app.get("/hello")
@@ -15,9 +15,9 @@ async def root() -> dict | list:
     return {"message": "Hello World"}
 
 
-@app.get("/calculate")
-async def calculate(a: int, b: int) -> dict:
-    return {"result": a + b}
+@app.get("/calculate", response_class=JSONResponse)
+async def calculate(a: int, b: int) -> JSONResponse:
+    return JSONResponse({"result": a + b}, status_code=200)
 
 
 if __name__ == "__main__":
